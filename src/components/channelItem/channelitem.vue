4ec7baec555b52f3c1cda63a8df0fd14c06ed316
lixiaowei <847458641@qq.com>
2020-12-18 21:23:43 +0800

    完成样式修改

---
+++ src/components/channelItem/channelitem.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="channlitem">
+    <van-cell>
+      <div slot="title" class="title van-multi-ellipsis--l3">
+        {{ channlitem.title }}
+      </div>
+      <div slot="label">
+        <div class="imagesBox" v-if="channlitem.cover.type === 3">
+          <div
+            class="imagesitem"
+            v-for="(img, index) in channlitem.cover.images"
+            :key="index"
+          >
+            <van-image
+              class="sanImge van-hairline--surround"
+              fit="cover"
+              :src="img"
+            />
+          </div>
+        </div>
+        <span>{{ channlitem.aut_name }}</span>
+        <span>{{ channlitem.comm_count }}评论</span>
+        <span>{{ channlitem.pubdate }}</span>
+      </div>
+      <!-- 默认插槽default可以省略不写 -->
+      <!-- 三种情况 0,1,3-->
+      <van-image
+        class="rightImg"
+        v-if="channlitem.cover.type === 1"
+        fit="cover"
+        :src="channlitem.cover.images[0]"
+      />
+    </van-cell>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "channlitem",
+  props: {
+    channlitem: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 16px;
+  color: #3a3a3a;
+}
+.imagesBox {
+  display: flex;
+  .imagesitem {
+    flex: 1;
+    height: 73px;
+    .sanImge {
+      width: 100%;
+      height: 73px;
+    }
+  }
+}
+/deep/ .van-cell__value {
+  width: 116px;
+  height: 73px;
+  flex: unset;
+  margin-left: 10px;
+}
+span {
+  margin-right: 12px;
+  letter-spacing: 2px;
+}
+.rightImg {
+  width: 116px;
+  height: 73px;
+}
+</style>
--- src/components/channels.vue
+++ src/components/channels.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="channels">
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多数据了！"
-      @load="onLoad"
+    <van-pull-refresh
+      :success-text="successText"
+      v-model="refreshing"
+      @refresh="onRefresh"
     >
-      <van-button size="small" round icon="back-top"></van-button>
-      <van-cell
-        class="vanCell"
-        v-for="(contentItem, index) in content"
-        :key="index"
-        :title="contentItem.title"
-      />
-    </van-list>
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多数据了！"
+        @load="onLoad"
+        @refresh="onRefresh"
+      >
+        <van-button v-if="show" size="small" round icon="back-top"></van-button>
+        <channlitem
+          v-for="(contentItem, index) in content"
+          :key="index"
+          :channlitem="contentItem"
+        />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
 <script>
 import { onLoadGetContent } from "../api/user";
+import channlitem from "@/components/channelItem/channelitem";
 export default {
   name: "channels",
   props: {
@@ -27,13 +34,19 @@ export default {
       required: true, // 是否必须
     },
   },
+  components: {
+    channlitem,
+  },
   // props: ['item'],不建议这样写
   data() {
     return {
       loading: false,
       finished: false,
+      refreshing: false,
       content: [],
       timestamp: null, // 获取下一页的时间戳
+      show: false,
+      successText: "",
     };
   },
   created() {},
@@ -48,17 +61,28 @@ export default {
       });
       // 将数组合并-方法一：concat，方法二：...str
       this.content.push(...data.data.results);
-      // console.log(this.content);
+      console.log(this.content);
       // 加载结束
       this.loading = false;
       // 如果还有数据
       if (this.content.length) {
         // 获取下一页数据
         this.timestamp = data.data.pre_timestamp;
+        this.show = true;
       } else {
         // 加载状态结束，不再触发加载更多
         this.finished = true;
       }
+    },
+    async onRefresh() {
+      const { data } = await onLoadGetContent({
+        channel_id: this.item.id, // 频道ID
+        timestamp: Date.now(), // 时间戳
+        with_top: 1, // 是否置顶
+      });
+      this.content.unshift(...data.data.results);
+      this.refreshing = false;
+      this.successText = `成功刷新,获取到${data.data.results.length}条新数据啦！`;
     },
   },
 };
